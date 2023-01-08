8d3998ea77f3b62fb44a9bea051ae1619410be8c
dedeheri <dedeherii60@gmail.com>
2023-01-09 01:06:32 +0700

    fixed bug

--- index.js
+++ index.js
@@ -40,6 +40,8 @@ e.use(helmet());
 e.use(
   session({
     secret: "yoursecret",
+    resave: true,
+    saveUninitialized: true,
     cookie: {
       path: "/",
       domain: process.env.CLIENT_URL,
@@ -47,7 +49,7 @@ e.use(
     },
   })
 );
-app.use(function (req, res, next) {
+e.use(function (req, res, next) {
   res.header("Access-Control-Allow-Credentials", true);
   res.header("Access-Control-Allow-Origin", req.headers.origin);
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
